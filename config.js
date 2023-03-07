1453489e3d3b1d4e0ecd91f5265a03b3744c4254
DucAnh21092000 <dinhanh21092000@gmail.com>
2023-03-07 23:58:50 +0700

    done

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Chúc bé yêu 8/3 vui vẻ",
+
+    background: "background.jpg",
+
+    min1: "26.jpg",
+    min2: "13.jpg",
+    min3: "14.jpg",
+    min4: "15.jpg",
+    min5: "17.jpg",
+    min6: "18.jpg",
+
+    max1: "21.jpg",
+    max2: "19.jpg",
+    max3: "22.jpg",
+    max4: "23.jpg",
+    max5: "24.jpg",
+    max6: "20.jpg",
+}
